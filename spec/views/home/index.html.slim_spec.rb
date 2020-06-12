986f917156ed8800cfc1b7e5a28ab260f15f2283
aliterm <me@situsali.id>
2020-06-12 12:40:27 +0700

    Add home controller and index view

---
+++ spec/views/home/index.html.slim_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe 'home/index.html.slim', type: :view do
+  it 'renders default' do
+    render
+    expect(rendered).to have_selector 'section.hero.is-primary .hero-body .container', count: 1
+  end
+end
