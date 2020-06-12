eca0ed6a934e7d7e2a8330b08ca90d54908742d2
aliterm <me@situsali.id>
2020-06-12 14:50:39 +0700

    Writing model test for user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,44 @@
-require 'rails_helper'
-
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.create :user }
+
+  describe 'validates' do
+    context '#email' do
+      it 'valid when email is present' do
+        expect(user.email).to be_present
+        expect(user).to be_valid
+      end
+
+      it 'invalid when email is not present' do
+        user.update(email: nil)
+        expect(user.email).to be_nil
+        expect(user).to be_invalid
+      end
+    end
+
+    context '#username' do
+      it 'valid when username is present' do
+        expect(user.username).to be_present
+        expect(user).to be_valid
+      end
+
+      it 'invalid when username is not present' do
+        user.update(username: nil)
+        expect(user.username).to be_nil
+        expect(user).to be_invalid
+      end
+    end
+
+    context '#password' do
+      it 'minimum 8 and maximum 128 characters' do
+        expect(user.password.length).to be >= 8
+        expect(user.password.length).to be <= 128
+
+        user.update(password: 'passwo')
+        expect(user).to be_invalid
+
+        user.update(password: 'pass' * 50)
+        expect(user).to be_invalid
+      end
+    end
+  end
 end
