24e5d56aeebf87d4aa8f20c54dfb32c24048d63f
aliterm <me@situsali.id>
2020-06-12 13:58:19 +0700

    Add factories for user

---
+++ spec/factories/user.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :user do
+    name     { Faker::Name.name }
+    username { name.parameterize(separator: '_') }
+    email    { Faker::Internet.email }
+    password { 'Password' }
+
+    trait :confirmed do
+      confirmed_at { DateTime.now.to_formatted_s(:db) }
+    end
+  end
+end
