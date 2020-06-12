843ad742a8a24d0ca11e862d1dd58035d6df1779
aliterm <me@situsali.id>
2020-06-12 12:40:38 +0700

    Change routes for root path with home/index

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -1,12 +1,8 @@
-require 'rails_helper'
-
-RSpec.describe "Homes", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/home/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Home', type: :request do
+  describe 'GET root path' do
+    it 'returns http success' do
+      get '/'
+      expect(response).to have_http_status :success
     end
   end
-
 end
