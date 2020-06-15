b102f1fc517460a8906e7273ad06f338e5cd9f34
aliterm <me@situsali.id>
2020-06-15 09:48:40 +0700

    Add request helper for devise login

--- spec/requests/admin_request_spec.rb
+++ spec/requests/admin_request_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe 'Admins', type: :request do
 
   context 'when user is log in' do
     it 'also return 400 when user is not admin' do
+      sign_in FactoryBot.create :user
       get '/admin'
       expect(response).to have_http_status :not_found
     end
