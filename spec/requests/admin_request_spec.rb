0001766fd15847b6ab71b990ced64d5f5a88fe5e
aliterm <me@situsali.id>
2020-06-15 10:02:03 +0700

    Fix request test for admin

--- spec/requests/admin_request_spec.rb
+++ spec/requests/admin_request_spec.rb
@@ -2,17 +2,23 @@
 
 RSpec.describe 'Admins', type: :request do
   context 'when user is not log in' do
-    it 'returns 404' do
+    it 'returns 404 (not found)' do
       get '/admin'
       expect(response).to have_http_status :not_found
     end
   end
 
   context 'when user is log in' do
-    it 'also return 400 when user is not admin' do
+    it 'Returns 404 (not found) if user is not admin' do
       sign_in FactoryBot.create :user
       get '/admin'
       expect(response).to have_http_status :not_found
+    end
+
+    it 'returns 200 if user is admin' do
+      sign_in FactoryBot.create :admin_user
+      get '/admin'
+      expect(response).to have_http_status :ok
     end
   end
 end
