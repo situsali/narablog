3a7510ffd001d02647abdffabd4dab72d7962278
aliterm <me@situsali.id>
2020-06-15 17:27:52 +0700

    Fix devise registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def render_404
     respond_to do |format|
       format.html { render file: "#{Rails.root}/public/404.html", layout: false, status: :not_found }
       format.xml  { head :not_found }
       format.any  { head :not_found }
     end
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    attributes = [:name, :username]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
+    devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
 end
