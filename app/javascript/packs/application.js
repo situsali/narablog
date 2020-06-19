21507c5defebccb2c895cb116246e99e0590e7b1
aliterm <me@situsali.id>
2020-06-19 09:56:44 +0700

    Styling admin page using sidebar navigation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,16 @@ import '@mdi/font/scss/materialdesignicons'
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+document.addEventListener('DOMContentLoaded', function () {
+  var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(function ($el) {
+      $el.addEventListener('click', function () {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        $el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+      });
+    });
+  }
+});
