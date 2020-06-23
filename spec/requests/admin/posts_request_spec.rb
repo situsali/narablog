dcae8ad4021865a1c12fc7bef9411962c85f098e
aliterm <me@situsali.id>
2020-06-23 09:11:00 +0700

    Fix the red test and skip the test that not implemented yet

--- spec/requests/admin/posts_request_spec.rb
+++ spec/requests/admin/posts_request_spec.rb
@@ -1,54 +1,71 @@
-require 'rails_helper'
-
-RSpec.describe "Admin::Posts", type: :request do
-
-  describe "GET /index" do
-    it "returns http success" do
-      get "/admin/posts/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Admin::Posts', type: :request do
+  context 'when user is not login' do
+    it 'returns http not found' do
+      get admin_posts_path
+      expect(response).to have_http_status :not_found
     end
   end
 
-  describe "GET /show" do
-    it "returns http success" do
-      get "/admin/posts/show"
-      expect(response).to have_http_status(:success)
+  context 'when logged in user is not admin' do
+    it 'returns http not found' do
+      sign_in FactoryBot.create :user, :confirmed
+      get admin_posts_path
+      expect(response).to have_http_status :not_found
     end
   end
 
-  describe "GET /edit" do
-    it "returns http success" do
-      get "/admin/posts/edit"
-      expect(response).to have_http_status(:success)
+  context 'when admin user is admin' do
+    let(:admin_user) { FactoryBot.create :admin_user }
+
+    before { sign_in admin_user }
+
+    describe 'GET /index' do
+      it 'returns http success' do
+        get '/admin/posts/index'
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    xdescribe "GET /show" do
+      it "returns http success" do
+        get admin_post_path id: 1
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    xdescribe "GET /edit" do
+      it "returns http success" do
+        get "/admin/posts/edit"
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    xdescribe "GET /new" do
+      it "returns http success" do
+        get "/admin/posts/new"
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    xdescribe "GET /create" do
+      it "returns http success" do
+        get "/admin/posts/create"
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    xdescribe "GET /update" do
+      it "returns http success" do
+        get "/admin/posts/update"
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    xdescribe "GET /destroy" do
+      it "returns http success" do
+        get "/admin/posts/destroy"
+        expect(response).to have_http_status(:success)
+      end
     end
   end
-
-  describe "GET /new" do
-    it "returns http success" do
-      get "/admin/posts/new"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /create" do
-    it "returns http success" do
-      get "/admin/posts/create"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /update" do
-    it "returns http success" do
-      get "/admin/posts/update"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET /destroy" do
-    it "returns http success" do
-      get "/admin/posts/destroy"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
