7a62d6532ebca885dbd27dbad5ce169cb7e89d57
aliterm <me@situsali.id>
2020-06-24 11:15:18 +0700

    Goodbye Sprockets! Welcome Webpacker.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,8 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
-  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  # config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
@@ -110,4 +110,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  config.assets.css_compressor = :yui
+  config.assets.js_compressor = :uglifier
 end
