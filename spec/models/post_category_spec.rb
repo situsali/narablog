6a26e6d0e359a9484ee18669d50b5ac936d46d67
aliterm <me@situsali.id>
2020-06-29 09:57:22 +0700

    Create a test for PostCategory

--- spec/models/post_category_spec.rb
+++ spec/models/post_category_spec.rb
@@ -1,5 +1,19 @@
-require 'rails_helper'
-
 RSpec.describe PostCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validates of post and category' do
+    context 'when post is not set' do
+      it 'fails to create a post_category' do
+        pc = FactoryBot.build :post_category, post: nil
+        expect(pc).to be_invalid
+        expect(pc.errors.messages[:post].size).to be 1
+      end
+    end
+
+    context 'when category is not set' do
+      it 'fails to create a post_category' do
+        pc = FactoryBot.build :post_category, category: nil
+        expect(pc).to be_invalid
+        expect(pc.errors.messages[:category].size).to be 1
+      end
+    end
+  end
 end
