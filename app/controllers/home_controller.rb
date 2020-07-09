61591eb8df989f4da056e0b49fb07ea846384dbf
aliterm <me@situsali.id>
2020-07-09 13:15:20 +0700

    Routing for Home Posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    posts = Post.where(published: true)
+    render :index, locals: { posts: posts }
+  end
+
+  def post
+    post = Post.find_by slug: params[:id]
+    render :post, locals: { post: post }
   end
 end
