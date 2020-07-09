aa26683c72a4f6b5765b50ba4ca6b2e8d335258a
aliterm <me@situsali.id>
2020-07-09 10:12:10 +0700

    Implement rouge for syntax highlighter in markdown

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -10,6 +10,8 @@ require("src/javascripts/channels")
 
 import "bulma"
 import '@mdi/font/scss/materialdesignicons'
+import '../src/stylesheets/syntax.css'
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
