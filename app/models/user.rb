07b2abf5b602efa6614b93fc9c600c612a4d40bc
aliterm <me@situsali.id>
2020-07-21 08:59:39 +0700

    Login/Register Google Socials Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,15 @@ class User < ApplicationRecord
             presence: true,
             length: { minimum: 5, maximum: 128 },
             format: { with: /\A[a-zA-Z0-9_]+\Z/ }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider,
+          uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name
+      user.username = user.name.parameterize(separator: '_')[0..128]
+      user.skip_confirmation!
+    end
+  end
 end
