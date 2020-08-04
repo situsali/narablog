6d760593ea8bdd017bfd1363e531b1c7ee333955
aliterm <me@situsali.id>
2020-08-04 14:23:28 +0700

    Change media storedir and add valid mimetype

--- app/uploaders/media_uploader.rb
+++ app/uploaders/media_uploader.rb
@@ -10,7 +10,7 @@ class MediaUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.pluralize.underscore}/#{model.created_at.strftime('%Y/%m')}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
@@ -35,9 +35,9 @@ class MediaUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png pdf)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
