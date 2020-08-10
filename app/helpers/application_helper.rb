edfa0c7be77f3ecda867abedc310a4024f43fafe
aliterm <me@situsali.id>
2020-08-10 13:47:17 +0700

    Add variable env for custom sitename and create dynamic page title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  def page_title(page_title = '')
+    ENV['SITENAME'] = 'Narablog' unless ENV['SITENAME'].present?
+
+    title = sitename = ENV['SITENAME']
+    title = "#{sitename} - #{page_title}" if page_title.present?
+    content_for :title, title
+  end
 end
 
 class String
