9fb629ac5ba666c5c1703d5aa50553436eb423fd
aliterm <me@situsali.id>
2020-08-15 13:02:12 +0700

    CRUD for static page in admin panel

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,22 +1,62 @@
 class Admin::PagesController < AdminController
+  rescue_from ActiveRecord::InvalidForeignKey, with: :cannot_delete
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def index
+    render :index, locals: { pages: Page.all }
   end
 
   def show
+    @page = current_page
   end
 
   def new
-  end
-
-  def edit
+    @page = Page.new
   end
 
   def create
+    @page = Page.create page_params
+    return render :new unless @page.valid?
+
+    redirect_to admin_pages_path
+  end
+
+  def edit
+    @page = current_page
   end
 
   def update
+    @page = current_page
+
+    if @page.update page_params
+      redirect_to admin_pages_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+    page = current_page
+    page.destroy
+    redirect_to admin_pages_path,
+      notice: "Delete #{page.title} is success"
+  end
+
+  private
+
+  def current_page
+    @current_page ||= Page.find params[:id]
+  end
+
+  def cannot_delete
+    redirect_to admin_pages_path, notice: 'errors'
+  end
+
+  def record_not_found
+    redirect_to admin_pages_path, notice: 'Page not found'
+  end
+
+  def page_params
+    params.require(:page).permit(:title, :slug, :body)
   end
 end
--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,14 @@
 class Page < ApplicationRecord
+  validates :title, presence: true
+  validates :slug, presence: true, format: { with: /\A[a-zA-Z0-9_-]+\Z/ }
+
+  before_validation :generate_slug
+
+  private
+
+  def generate_slug
+    return if title.blank?
+
+    self.slug = title.parameterize if slug.blank?
+  end
 end
